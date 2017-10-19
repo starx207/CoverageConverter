340d59152429f45678e9f2b949b97d59554064b6
Andrew Starr <astarr@appraisalresearch.cc>
2017-10-19 15:42:50 -0400

    Initial Commit

---
+++ CoverageConverter/CoverageConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Coverage.Analysis;
+
+namespace CoverageConverter {
+    class Program {
+        static void Main(string[] args) {
+            if (args == null || args.Length != 3 || args[0] == null || args[1] == null || args[2] == null ||
+                string.IsNullOrEmpty(args[0]) || string.IsNullOrEmpty(args[1]) || string.IsNullOrEmpty(args[2])) {
+
+                help();
+                return;
+            }
+
+            string coverageFileName = args[0];
+            string dllFileName = args[1];
+            string coverageXmlFileName = args[2];
+
+            Console.WriteLine("args[0] = " + coverageFileName);
+            Console.WriteLine("args[1] = " + dllFileName);
+            Console.WriteLine("args[2] = " + coverageXmlFileName);
+
+            using (CoverageInfo info = CoverageInfo.CreateFromFile(coverageFileName, new string[] { dllFileName }, new string[] { })) {
+                CoverageDS data = info.BuildDataSet();
+                data.WriteXml(coverageXmlFileName);
+            }
+        }
+
+        static private void help() {
+            Console.WriteLine("Help!");
+            Console.Write(@"
+                Arguments
+                    Args[0]: data coverage file name (data.coverage)
+                    Args[1]: DLL to use (myProject.dll)
+                    Args[2]: coveragexml file name to be generated (converted.coveragexml)
+            ");
+        }
+    }
+}
